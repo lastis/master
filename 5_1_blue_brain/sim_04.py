78d75534ab62233cf4508f66c7da053b20324a70
lastis <neitakkikkebanan@gmail.com>
2016-01-22 14:43:02 +0100

    update.

--- 5_1_blue_brain/sim_04.py
+++ 5_1_blue_brain/sim_04.py
@@ -3,12 +3,11 @@ import LFPy_util
 import os
 import sys
 from glob import glob
-from multiprocessing import Process
 
 cores = 1
 output_dir = "sim_04"
 # How many neurons from each group to simulate.
-nrn_cnt = 1
+nrn_cnt = 2
 
 # Gather directory paths.
 dir_model = blue_brain.dir_model
@@ -21,7 +20,7 @@ TTPC1 = glob('L5_*TTPC1*')[:nrn_cnt]
 NBC = glob('L5_*NBC*')[:nrn_cnt]
 
 # Gather neurons to be simulated.
-neurons = TTPC1 + NBC
+neurons = TTPC1
 
 # Process command input.
 simulate = False
@@ -43,52 +42,77 @@ blue_brain.download_all_models(dir_model)
 if simulate:
     mod_dir = os.path.join(blue_brain.dir_res,'extra_mod/')
     LFPy_util.nrnivmodl(mod_dir)
-
-# Define a simulation method so different neurons can be run in parallel.
-def run(nrn_full):
-    # Load cell objects from bluebrain.
+    
+# Load cell objects from bluebrain.
+def load_func(neuron):
+    nrn_full = os.path.join(dir_model,neuron)
     cell_list = blue_brain.load_model(nrn_full)
     cell = cell_list[0]
+    return cell
 
-    sh = LFPy_util.SimulationHelper()
-    sh.set_cell(cell)
-    sh.set_dir_neurons(dir_neurons)
-    sh.set_neuron_name(nrn)
-    print sh
+sh = LFPy_util.SimulationHelper()
+sh.set_cell_load_func(load_func)
+sh.set_dir_neurons(dir_neurons)
+sh.set_neuron_name(neurons)
+sh.simulate = simulate
+sh.plot = plot
 
-    # Configure simulation objects.
-    sim_single_spike    = LFPy_util.sims.SingleSpike()
-    sim_single_spike.prev_data = sh.get_path_data(sim_single_spike)
-    sim_single_spike.run_param['pptype'] = 'ISyn'
-    sim_morph           = LFPy_util.sims.Morphology()
-    sim_sphere = LFPy_util.sims.SphereElectrodes()
+sim_single_spike    = LFPy_util.sims.SingleSpike()
+sim_single_spike.prev_data = sh.get_path_sim_data(sim_single_spike)
+sim_single_spike.run_param['pptype'] = 'ISyn'
+sim_intra           = LFPy_util.sims.Intracellular()
 
-    # Find the principal component axes and rotate cell.
-    axes = LFPy_util.data_extraction.findMajorAxes()
-    # Aligns y to axis[0] and x to axis[1]
-    LFPy_util.rotation.alignCellToAxes(cell,axes[0],axes[1])
+sh.push(sim_single_spike,False)
+sh.push(sim_intra,False)
 
-    sh.push(sim_single_spike,False)
-    sh.push(sim_morph,True)
-    sh.push(sim_sphere,True)
+print sh
+sh.run()
 
-    if simulate:
-        sh.simulate()
-    if plot:
-        sh.plot()
+# # Define a simulation method so different neurons can be run in parallel.
+# def run(nrn_full):
+#     # Load cell objects from bluebrain.
+#     cell_list = blue_brain.load_model(nrn_full)
+#     cell = cell_list[0]
+
+#     sh = LFPy_util.SimulationHelper()
+#     sh.set_cell(cell)
+#     sh.set_dir_neurons(dir_neurons)
+#     sh.set_neuron_name(nrn)
+#     print sh
+
+#     # Configure simulation objects.
+#     sim_single_spike    = LFPy_util.sims.SingleSpike()
+#     sim_single_spike.prev_data = sh.get_path_data(sim_single_spike)
+#     sim_single_spike.run_param['pptype'] = 'ISyn'
+#     sim_morph           = LFPy_util.sims.Morphology()
+#     sim_sphere = LFPy_util.sims.SphereElectrodes()
+
+#     # Find the principal component axes and rotate cell.
+#     axes = LFPy_util.data_extraction.findMajorAxes()
+#     # Aligns y to axis[0] and x to axis[1]
+#     LFPy_util.rotation.alignCellToAxes(cell,axes[0],axes[1])
+
+#     sh.push(sim_single_spike,False)
+#     # sh.push(sim_morph,True)
+#     sh.push(sim_sphere,False)
+
+#     if simulate:
+#         sh.simulate()
+#     if plot:
+#         sh.plot()
 
 # Start simulation(s)
-if simulate or plot:
-    p_arr = []
-    for cnt, nrn in enumerate(neurons):
-        nrn_full = os.path.join(dir_model,nrn)
-        p = Process(target=run, args=(nrn_full,))
-        p.start()
-        p_arr.append(p)
-        # Wait if the number of simulations reaches the number of cores.
-        if cnt % cores == cores-1:
-            for p in p_arr:
-                p.join()
-            p_arr = []
-    for p in p_arr:
-        p.join()
+# if simulate or plot:
+#     p_arr = []
+#     for cnt, nrn in enumerate(neurons):
+#         nrn_full = os.path.join(dir_model,nrn)
+#         p = Process(target=run, args=(nrn_full,))
+#         p.start()
+#         p_arr.append(p)
+#         # Wait if the number of simulations reaches the number of cores.
+#         if cnt % cores == cores-1:
+#             for p in p_arr:
+#                 p.join()
+#             p_arr = []
+#     for p in p_arr:
+#         p.join()
