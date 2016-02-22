9aec90ebaa1f42d7b1a983467a89b371c612551e
lastis <neitakkikkebanan@gmail.com>
2016-02-22 15:51:03 +0100

    More files.

--- 5_3_exp_data/handler.py
+++ 5_3_exp_data/handler.py
@@ -1,9 +1,15 @@
+import os
 import urllib
+import neo
 
 URL_SPIKE_DATA = 'http://folk.uio.no/danielmb/spike_data.h5'
 PATH_SPIKE_DATA = 'spike_data.h5'
 
 def download_data():
-    opener = urllib.URLopener()
-    print "Downloading: " + URL_SPIKE_DATA
-    opener.retrieve(URL_SPIKE_DATA, PATH_SPIKE_DATA)
+    if not os.path.isfile(PATH_SPIKE_DATA):
+        print "Downloading: " + URL_SPIKE_DATA
+        urllib.urlretrieve(URL_SPIKE_DATA, PATH_SPIKE_DATA)
+
+    reader = neo.io.NeoHdf5IO(PATH_SPIKE_DATA)
+    blocks = reader.read()
+    print blocks
--- 5_3_exp_data/readme.md
+++ 5_3_exp_data/readme.md
@@ -1,5 +1,9 @@
 Install dependencies with:
+
 ```
 pip install -r requirements.txt
 ```
+
+Ubuntu requires:
+* `libhdf5-dev`.
 
