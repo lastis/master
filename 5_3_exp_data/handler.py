d94e9917482ebfe8ac4864f600f988f88f11c585
lastis <neitakkikkebanan@gmail.com>
2016-02-22 16:12:39 +0100

    update

--- 5_3_exp_data/handler.py
+++ 5_3_exp_data/handler.py
@@ -1,3 +1,7 @@
+"""
+Collection of function to setup necessary files.
+"""
+
 import os
 import urllib
 import neo
@@ -5,11 +9,22 @@ import neo
 URL_SPIKE_DATA = 'http://folk.uio.no/danielmb/spike_data.h5'
 PATH_SPIKE_DATA = 'spike_data.h5'
 
+
 def download_data():
+    """
+    Download experimental data files.
+    """
     if not os.path.isfile(PATH_SPIKE_DATA):
         print "Downloading: " + URL_SPIKE_DATA
         urllib.urlretrieve(URL_SPIKE_DATA, PATH_SPIKE_DATA)
 
+
+def get_data():
+    """
+    Return neo blocks.
+
+    Can be iterated with for loop returning a block.
+    """
     reader = neo.io.NeoHdf5IO(PATH_SPIKE_DATA)
     blocks = reader.read()
-    print blocks
+    return blocks
--- 5_3_exp_data/test.py
+++ 5_3_exp_data/test.py
@@ -1,3 +1,10 @@
+# pylint: disable=invalid-name, missing-docstring
 import handler
+import numpy as np
 
 handler.download_data()
+blocks = handler.get_data()
+
+for block in blocks:
+    for rec in block.recordingchannelgroups:
+        print rec
